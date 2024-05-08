f156c5796ddda4c08ed6598beaf14e061eeb68be
Stephany Vargas <vargas@mi-6.co.jp>
2024-05-08 09:41:41 +0900

    dependencies working!!!!

--- Chatbot.py
+++ Chatbot.py
@@ -1,4 +1,5 @@
 __import__('pysqlite3')
+import pysqlite3
 import sys
 sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
@@ -18,3 +19,37 @@ retriever_materials = vectordb_materials.as_retriever() # search_kwargs={"k": 2}
 docs = retriever_materials.invoke("what are high functional materials?")
 
 st.write(docs)
+
+# full Q&A
+from langchain.chains import RetrievalQA
+from langchain_openai import OpenAI
+
+llm=OpenAI()
+# create the chain to answer questions
+qa_chain = RetrievalQA.from_chain_type(llm=OpenAI(),
+                                  chain_type="stuff",
+                                  retriever=retriever_materials,
+                                  return_source_documents=True)
+## Cite sources
+def process_llm_response(llm_response):
+  print('---- Query ---')
+  print(llm_response['query'],'\n')
+
+  print('---- Answer ---')
+  answer = llm_response['result']
+  print(textwrap.fill(answer, 80))
+
+  print('\n\n ---- Sources -----')
+  for source in llm_response["source_documents"]:
+      print(source.metadata['title'])
+      print(source.metadata['DOI'])
+  print("-----------")
+
+  return answer
+
+# full Q&A
+
+query = "what are high functional materials?"
+llm_response = qa_chain(query)
+ans1 = process_llm_response(llm_response)
+st.write(ans1)
